fc3cad92574b76d27c629b704f23c70ef46a2428
Jeroen Domburg <jeroen@spritesmods.com>
2016-01-21 16:21:47 +0800

    Change to send routine: return status

--- core/httpd-platform.h
+++ core/httpd-platform.h
@@ -1,7 +1,7 @@
 #ifndef HTTPD_PLATFORM_H
 #define HTTPD_PLATFORM_H
 
-void httpdPlatSendData(ConnTypePtr conn, char *buff, int len);
+int httpdPlatSendData(ConnTypePtr conn, char *buff, int len);
 void httpdPlatDisconnect(ConnTypePtr conn);
 void httpdPlatInit(int port, int maxConnCt);
 
