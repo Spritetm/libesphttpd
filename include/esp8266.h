5d043f064790c62432e806396a164b7c74c9b42e
David Betz <david.betz@st.com>
2018-02-06 15:41:39 -0500

    Open all files in binary mode in mkroffsimage.

--- include/esp8266.h
+++ include/esp8266.h
@@ -31,4 +31,18 @@
 
 #include "platform.h"
 #include "espmissingincludes.h"
+#if 0
+void *ets_memcpy(void *dest, const void *src, size_t n);
+void *ets_memset(void *s, int c, size_t n);
+int ets_strcmp(const char *s1, const char *s2);
+int ets_strncmp(const char *s1, const char *s2, int len);
+size_t ets_strlen(const char *s);
+char *ets_strcpy(char *dest, const char *src);
+char *ets_strstr(const char *haystack, const char *needle);
+int ets_sprintf(char *str, const char *format, ...)  __attribute__ ((format (printf, 2, 3)));
+int os_printf_plus(const char *format, ...)  __attribute__ ((format (printf, 1, 2)));
+int strcasecmp(const char *a, const char *b);
 
+void *pvPortMalloc(size_t xWantedSize, const char *file, int line);
+void vPortFree(void *ptr, const char *file, int line);
+#endif
